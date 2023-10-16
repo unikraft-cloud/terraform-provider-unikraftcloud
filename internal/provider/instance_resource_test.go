c17bb12e51b388ab8f7aa38f782bc642f35085a7
Antoine Cotten <antoine@unikraft.io>
2023-10-16 18:58:18 +0200

    fix(test): propagate instance uuid to ImportState
    Signed-off-by: Antoine Cotten <antoine@unikraft.io>

--- internal/provider/instance_resource_test.go
+++ internal/provider/instance_resource_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 func TestAccInstanceResource(t *testing.T) {
@@ -31,13 +32,17 @@ func TestAccInstanceResource(t *testing.T) {
 			},
 			// ImportState testing
 			{
-				ResourceName:      "kraftcloud_instance.test",
-				ImportState:       true,
-				ImportStateVerify: true,
-				// FIXME(antoineco): ImportState still receives req.ID="id-attribute-not-set"
-				// despite this option, and despite the attribute having UseStateForUnknown
-				// set in the schema.
+				ResourceName:                         "kraftcloud_instance.test",
+				ImportState:                          true,
+				ImportStateVerify:                    true,
 				ImportStateVerifyIdentifierAttribute: "uuid",
+				ImportStateIdFunc:                    instanceUUID("kraftcloud_instance.test"),
+				ImportStateVerifyIgnore: []string{
+					// not returned for instances in "stopped" state
+					"port", "internal_port", "handlers",
+					// differs from given value if the image references a tag (vs. a digest)
+					"image",
+				},
 			},
 			// Update and Read testing
 			{
@@ -58,4 +63,23 @@ resource "kraftcloud_instance" "test" {
   port  = %[2]d
 }
 `, imageAttr, portAttr)
+}
+
+// instanceUUID returns a resource.ImportStateIdFunc which retrieves the uuid
+// attribute of the given "instance" resource from the Terraform state.
+// This is used in ImportState tests, because resources of type "instance" must
+// be imported using their uuid.
+func instanceUUID(resourceName string) resource.ImportStateIdFunc {
+	const attrName = "uuid"
+	return func(st *terraform.State) (string, error) {
+		res, ok := st.RootModule().Resources[resourceName]
+		if !ok {
+			return "", fmt.Errorf("could not find resource %q in Terraform state", resourceName)
+		}
+		attr, ok := res.Primary.Attributes[attrName]
+		if !ok {
+			return "", fmt.Errorf("attribute %q not set on resource %q", attrName, resourceName)
+		}
+		return attr, nil
+	}
 }
