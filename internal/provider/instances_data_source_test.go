a3446336cb11e13c2a560b9a99fb2badcd5374af
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2024-08-06 16:22:22 +0300

    fix: Remove unusable tests
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@gmail.com>

--- internal/provider/instances_data_source_test.go
+++ internal/provider/instances_data_source_test.go
@@ -4,66 +4,8 @@
 package provider
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccInstancesDataSource(t *testing.T) {
-	// Pre-existing "golden" instance, used exclusively for acceptance testing.
-	// User: robot$acotten.unikraft.io.users.unikraft-cloud
-	const tUUID = "3ce45bbf-5921-4590-ba1f-611da83871a0"
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			// Read testing
-			{
-				Config: `data "unikraft-cloud_instances" "test" {}`,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.unikraft-cloud_instances.test", "uuids.#", "1"),
-					resource.TestCheckResourceAttr("data.unikraft-cloud_instances.test", "uuids.0", tUUID),
-				),
-			},
-		},
-	})
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			// Read testing
-			{
-				Config: testAccInstancesDataSourceConfig(`["stopped", "stopping"]`),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.unikraft-cloud_instances.test", "uuids.#", "1"),
-					resource.TestCheckResourceAttr("data.unikraft-cloud_instances.test", "uuids.0", tUUID),
-				),
-			},
-		},
-	})
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			// Read testing
-			{
-				Config: testAccInstancesDataSourceConfig(`["running", "starting"]`),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.unikraft-cloud_instances.test", "uuids.#", "0"),
-				),
-			},
-		},
-	})
-}
-
-func testAccInstancesDataSourceConfig(statesAttr string) string {
-	return fmt.Sprintf(`
-data "unikraft-cloud_instances" "test" {
-  states = %[1]s
-}
-`, statesAttr)
 }
